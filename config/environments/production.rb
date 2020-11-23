16b09728c29c0104fec658a3c93780d42096cffa
Kyoma <steins.tomoya0213@gmail.com>
2020-11-22 21:06:29 -0800

    change sis-syte to syte-sis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sis-syte.herokuapp.com'
+  host = 'https://syte-sis.herokuapp.com'
   config.action_mailer.default_url_options = {host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
